c463e91be58b7d9cd6f25d0967fd1a6d0e6c67b2
Indrajeet <indrajeetkumarnayak3@gmail.com>
2024-01-12 21:49:36 +0530

    write code for update product

--- controller/productController.js
+++ controller/productController.js
@@ -3,17 +3,32 @@ const Product=require("../models/product")
 module.exports.createProduct=async(req,res)=>{
 try{
     const{name,desc,price,variants}=req.body;
-    const newProduct=await Product.create({
+    const Product=await Product.create({
         name,
         desc,
         price,
         variants
     })
 
-    res.stats(201).json(newProduct)
+    res.stats(201).json(Product)
 
 }catch(err){
 res.status(500).json({err:"Internal Server error"})
 }
 
+}
+module.exports.updateProduct=async(req,res)=>{
+try{
+    const{name,desc,price,variants}=req.body;
+    const updatedProduct=await Product.findByIdAndUpdate(
+        req.params.productId,
+        {
+            name,desc,price,variants
+        },
+        {new:true}
+        );
+        res.status(200).json(updatedProduct);
+}catch(err){
+res.status(500).json({err:"server error"})
+}
 }
