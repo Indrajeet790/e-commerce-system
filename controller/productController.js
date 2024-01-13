929871219331b0a3c715a1fc197b5bbab4593a08
Indrajeet <indrajeetkumarnayak3@gmail.com>
2024-01-13 14:30:30 +0530

    getAllProduct

--- controller/productController.js
+++ controller/productController.js
@@ -18,3 +18,15 @@ module.exports.createProduct = async (req, res) => {
       });
     }
   };
+
+  
+//get product
+module.exports.getAllProducts = async (req, res) => {
+    try {
+      const products = await Product.find();
+      res.status(200).json({ status: "success", products });
+    } catch (error) {
+      res.status(500).json({ status: "failed", error: error.message });
+    }
+  };
+  
