b15216bd09bcaf06291e343b6f9bff0ca34e4d09
Indrajeet <indrajeetkumarnayak3@gmail.com>
2024-01-13 14:26:21 +0530

    create a product

--- controller/productController.js
+++ controller/productController.js
@@ -1,60 +1,20 @@
 const Product=require("../models/product")
 
-
-// create a new product
-module.exports.createProduct=async(req,res)=>{
-try{
-    const{name,desc,price,variants}=req.body;
-    const Product=await Product.create({
-        name,
-        desc,
-        price,
-        variants
-    })
-
-    res.stats(201).json(Product)
-
-}catch(err){
-res.status(500).json({err:"Internal Server error"})
-}
-
-}
-// update a product
-module.exports.updateProduct=async(req,res)=>{
-try{
-    const{name,desc,price,variants}=req.body;
-    const updatedProduct=await Product.findByIdAndUpdate(
-        req.params.productId,
-        {
-            name,desc,price,variants
-        },
-        {new:true}
-        );
-        res.status(200).json(updatedProduct);
-}catch(err){
-res.status(500).json({err:"server error"})
-}
-}
-
-// view all product
-
-module.exports.findAllProduct=async(req,res)=>{
-    try{
-        const products=await Product.find().populate("variants");
-        res.status(200).json(products)
-    }catch(err){
-        res.status(500).json({err:"server error"})
+// write code for create a  product
+module.exports.createProduct = async (req, res) => {
+    try {
+      const product = new Product(req.body);
+      await product.save();
+      res.status(201).json({
+        status: "Success",
+        message: "Product Created Successfully",
+        product,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "failed",
+        message: "failed to create product",
+        error: error.message,
+      });
     }
-}
-
-// delete a product code
-module.exports.deleteProduct=async(req,res)=>{
-try{
-    const deletedProduct=await Product.findByIdAndDelete(
-        req.params.productId,
-        );
-        res.status(200).json(deletedProduct);
-}catch(err){
-res.status(500).json({err:"server error"})
-}
-}
+  };
